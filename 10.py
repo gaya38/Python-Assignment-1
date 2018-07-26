a4061d70d0b6c539de33e282049c7600b95509f6
Gayathri Ande (Product Engineering Service) <GA325352@wipro.com>
2018-07-26 17:54:09 +0530

    commited

---
+++ 10.py
@@ -0,0 +1,18 @@
+a=input("Enter a number:")
+b=input("Enter b number:")
+a+=b
+print "Addition of the numbers:",a
+a-=b
+print "Substraction of the numbers:",a
+a*=b
+print "Multiplication of the numbers:",a
+a/=b
+print "Division of the numbers:",a
+a%=b
+print "Modulus of the numbers:",a
+a**=b
+print "exponent of the numbers:",a
+a//=b
+print "exponent of the numbers:",a
+
+
