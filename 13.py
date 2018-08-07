d8309959e7adc01c1b4a71deff513a4aea6936f1
Gayathri Ande (Product Engineering Service) <GA325352@wipro.com>
2018-08-07 16:56:39 +0530

    commit

--- 13.py
+++ 13.py
@@ -3,15 +3,18 @@ for i in range(4):
     b=input("Enter a value:")
     a.append(b)            
 a.append(6)
+i=0
 print "Large number among the five values is:"
-for l in range(len(a)-1):
-        if (a[l]>a[l+1]):
-            temp=a[l]
-            a[l]=a[l+1]
-            a[l+1]=temp
-        else:
-            continue
-    
-print a[-1]
+if(a[i]>a[i+1] and a[i]>a[i+2] and a[i]>a[i+3] and a[i]>a[i+4]):
+    c=a[i]
+elif(a[i+1]>a[i+2] and a[i+1]>a[i+3] and a[i+1]>a[i+4]):
+    c=a[i+1]
+elif(a[i+2]>a[i+3] and a[i+2]>a[i+4]):
+    c=a[i+2]
+elif(a[i+3]>a[i+4]):
+    c=a[i+3]
+else:
+    c=a[i+4]
+print c
 
 
--- 5.py
+++ 5.py
@@ -1,30 +1,18 @@
-x=raw_input("Enter the program driver as arg or big:")
-if(x=='arg'):
-    a=raw_input("Enter a value:")
-    e=[]
-    g=0
-    for i in range(len(a)):
-        if (a[i]==" ")or(i==len(a)-1):
-            e.append(a[g:i+1])
-            g=i+1
+import sys
+x=len(sys.argv)
+a=[]
+i=1
+while(i<x):
+    a.append(sys.argv[i])
+    i=i+1
+print "The list of command line arguments:"
+print a
+for l in range(len(a)-1):
+        if (a[l]>a[l+1]):
+            temp=a[l]
+            a[l]=a[l+1]
+            a[l+1]=temp
         else:
             continue
-    for i in e:
-        print i
-elif(x=='big'):
-    b=input("Enter b value:")
-    k=[]
-    while(b>0):
-        k.append(b%10)
-        b=b/10
-    for l in range(len(k)-1):
-        if (k[l]>k[l+1]):
-            temp=k[l]
-            k[l]=k[l+1]
-            k[l+1]=temp
-        else:
-            continue
-    print k[-1]
-else:
-    print "Entered the wrong value so program ended"
-
+print "The large value among them:"
+print a[-1]
